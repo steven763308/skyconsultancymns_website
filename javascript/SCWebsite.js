a72cda133e87367afc621dbc790cc3901926b6e0
steven-skyconsultancy <steven@skyconsultancymns.com>
2025-04-09 00:53:43 +0800

    page update header part 2 (neat up UI)

--- javascript/SCWebsite.js
+++ javascript/SCWebsite.js
@@ -2,3 +2,8 @@ function toggleMenu(){
     const nav = document.getElementById('navBar');
     nav.classList.toggle('active');
 }
+
+function closeMenu(){
+    const nav = document.getElementById('navBar');
+    nav.classList.toggle('active');
+}
