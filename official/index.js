09ee2feebed40f5a14f3eac508981e993746b0eb
steven-skyconsultancy <steven@skyconsultancymns.com>
2025-04-09 01:42:08 +0800

    page setup phase 1 part 2 (header)

--- official/index.js
+++ official/index.js
@@ -1,8 +1,10 @@
+//open menu sidebar
 function toggleMenu(){
     const nav = document.getElementById('navBar');
     nav.classList.toggle('active');
 }
 
+//close menu sidebar
 function closeMenu(){
     const nav = document.getElementById('navBar');
     nav.classList.toggle('active');
